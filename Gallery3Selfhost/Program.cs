899080b83d52c87b478270ac366d6e9dc5ebe386
Jonathan Macgregor <52035222+DualWielder@users.noreply.github.com>
2020-04-01 12:25:31 +1300

    2 Host a Working ReST Service - Still working on

--- Gallery3Selfhost/Program.cs
+++ Gallery3Selfhost/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
 
 namespace Gallery3Selfhost
 {
@@ -7,6 +9,39 @@ namespace Gallery3Selfhost
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            // Set up server configuration
+
+            Uri _baseAddress = new Uri("http://localhost:60064/");
+
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+
+            config.Routes.MapHttpRoute(
+
+            name: "DefaultApi",
+
+            routeTemplate: "api/{controller}/{action}/{id}",
+
+            defaults: new { id = RouteParameter.Optional }
+
+            );
+
+            // Create server
+
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+
+            // Start listening
+
+            server.OpenAsync().Wait();
+
+            Console.WriteLine("Gallery Web-API Self hosted on " + _baseAddress);
+
+            Console.WriteLine("Hit ENTER to exit...");
+
+            Console.ReadLine();
+
+            server.CloseAsync().Wait();
+
         }
     }
 }
