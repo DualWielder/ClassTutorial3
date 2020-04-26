30c500c6a8334d013125fe2eb2b971b60cd17c00
Jonathan Macgregor <52035222+DualWielder@users.noreply.github.com>
2020-04-26 14:58:39 +1200

    5. Data Transfer Objects for a Flexible N-Tier Architecture

--- Gallery3WinForm/ServiceClient.cs
+++ Gallery3WinForm/ServiceClient.cs
@@ -11,10 +11,18 @@ namespace Gallery3WinForm
     public static class ServiceClient
     {
         internal async static Task<List<string>> GetArtistNamesAsync()
-        { 
+        {
             using (HttpClient lcHttpClient = new HttpClient())
                 return JsonConvert.DeserializeObject<List<string>>
-                    (await lcHttpClient.GetStringAsync("http://localhost:60064/api/gallery/GetArtistNames/")); 
+                    (await lcHttpClient.GetStringAsync("http://localhost:60064/api/gallery/GetArtistNames/"));
+        }
+
+        internal async static Task<clsArtist> GetArtistAsync(string prArtistName)
+        {
+            using (HttpClient lcHttpClient = new HttpClient())
+                return JsonConvert.DeserializeObject<clsArtist>
+                    (await lcHttpClient.GetStringAsync
+                        ("http://localhost:60064/api/gallery/GetArtist?Name=" + prArtistName));
         }
     }
 }
